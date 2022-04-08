06f63d0b575465453072640ea889644c887ed4c4
jngnandan <jngnandan@gmail.com>
2022-04-08 01:02:09 +0100

    second changes

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <ul class="px-4 flex flex-row justify-between items-center bg-gray-200 h-16 pt-1.5">
+                    <div>
+                        <Link className='px-4' to="/">
+                            <p>Logo</p>
+                        </Link>
+                        
+                    </div>
+
+                    <div>
+                        <Link className='px-4' to="/">Home</Link>
+                        <Link className='px-4' to="/about">About</Link>
+                        <Link className='px-4' to="/careers">Careers</Link>
+                        <Link className='px-4' to="/services">Services</Link>
+                        <Link className='px-4' to="/contactus">Contact</Link>
+                    </div>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react'
+
+export class NotFound extends Component {
+    render() {
+        return (
+            <div className='text-red-500'>NotFound</div>
+        )
+    }
+}
+
+export default NotFound
